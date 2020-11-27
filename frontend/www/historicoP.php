c7839f1042f32e30f245951817f8df8f1b7f2a71
Jaime García <74992736+JGarciaJ-00@users.noreply.github.com>
2020-11-27 01:05:18 +0100

    Update historicoP.php

--- frontend/www/historicoP.php
+++ frontend/www/historicoP.php
@@ -20,10 +20,10 @@
 				<form action="" method="POST"> 
 					<table>
 						<tr>
-						<p>Tu hijo ha realizado un ingredo en cuenta corriente de 300.000</p>
+						<p>Tu hijo ha realizado un ingreso en cuenta corriente de 300.000</p>
 						</tr>
 						<tr>
-						<p>Tu hijo ha realizado un ingredo en cuenta corriente de 300.000</p>
+						<p>Tu hijo ha realizado un ingreso en cuenta corriente de 300.000</p>
 						</tr>
 						<tr>
 						<p>¡Enhorabuena!, tu hijo ha adquirido un chalet en la Moraleja por valor de 600.000</p>
