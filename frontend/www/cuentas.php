47ef9a3b8dff3dc7bed4f62273d101a4fd6c104c
Jaime García <74992736+JGarciaJ-00@users.noreply.github.com>
2020-11-27 01:35:24 +0100

    Add files via upload

---
+++ frontend/www/cuentas.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<link href="https://fonts.googleapis.com/css?family=Ubuntu&display=swap" rel="stylesheet">
+<!--CSS-->
+		<link rel='stylesheet' href = "css/Comun.css" type = "text/css">
+		<link rel='stylesheet' href = "css/Historico.css" type = "text/css">
+	<title>Datos Historicos</title>
+	</head>
+	<Body>
+	<div id="contenedorGrande">
+		<div class="titulo"><img src=".\imagenes\LogoBBVA.png" class="LogoPeq"><h3>Cuentas de usuario hijo </h3></div>
+		<form id="back" name="back" method="post" action="./historicoH.php">
+			<input id="imgEdit" type="image" src=".\imagenes\Back01.png" alt="Smiley face">Atras
+		</form>
+
+
+
+		<form id="shop" name="shop" method="post" action="./tienda.php">
+			<input id="imgEdit2" type="image" src=".\imagenes\Tienda.png" alt="Smiley face">
+		</form>
+				<div id ="Cuerpo">
+				<form action="" method="POST">
+					<table>
+						<tr>
+						<p>ES0182 1234 5678 7894 1234 </p>
+						</tr>
+						<tr>
+						<p>ES0182 555 0062 7894 7451 </p>
+						</tr>
+					</table>
+				</form>
+			</div>
+		</div>
+</body>
